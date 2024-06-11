1be09e526e9d8f85163d8629b9dc471032d22e7c
Dimitri R-O <dimitri.rodriguesoliveira@prestataires.boulanger.com>
2024-06-11 11:22:28 +0200

    feat(*): add sms and coffee sequel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,29 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: async () => (await import('./pages/login/login.component')).LoginComponent,
+    loadComponent: async () => (await import('./pages/login/login.component')).LoginComponent
   },
   {
     path: 'home',
     pathMatch: 'full',
-    loadComponent: async () => (await import('./pages/home/home.component')).HomeComponent,
+    loadComponent: async () => (await import('./pages/home/home.component')).HomeComponent
   },
   {
     path: 'wait',
     pathMatch: 'full',
-    loadComponent: async () => (await import('./pages/wait/wait.component')).WaitComponent,
+    loadComponent: async () => (await import('./pages/wait/wait.component')).WaitComponent
   },
   {
     path: 'coffee',
     pathMatch: 'full',
-    loadComponent: async () => (await import('./pages/coffee/coffee.component')).CoffeeComponent,
+    loadComponent: async () => (await import('./pages/coffee/coffee.component')).CoffeeComponent
+  },
+  {
+    path: 'coffee/sequel',
+    pathMatch: 'full',
+    loadComponent: async () => (await import('./pages/coffee/sequel/coffee-sequel.component')).CoffeeSequelComponent
   }
 ];
