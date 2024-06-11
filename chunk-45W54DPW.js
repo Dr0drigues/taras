5cb62041baf7dc271c678691b25956452df11c15
dr0drigues <timmy.parkwayd@gmail.com>
2024-06-11 16:04:09 +0000

    Deployment from action (commit 11d252dae01fb0f9e24b91a90e7b50a0f9de9015)
    Triggered by commit: https://github.com/Dr0drigues/taras/commit/11d252dae01fb0f9e24b91a90e7b50a0f9de9015

---
+++ chunk-45W54DPW.js
@@ -0,0 +1,56 @@
+var e="login",a=`
+<p>
+  Th\xE9o et Romain \xE9taient deux \xE2mes cr\xE9atives, chacune attir\xE9e par la beaut\xE9 brute et mall\xE9able de l'argile.
+  Ils se sont rencontr\xE9s pour la premi\xE8re fois lors d\u2019une pause caf\xE9 revigorante dans un charmant village nomm\xE9 Marcq-en Baroeul.
+  L'atelier, tenu par un ma\xEEtre potier renomm\xE9, \xE9tait un lieu de calme et d'inspiration, o\xF9 les participants pouvaient laisser libre cours \xE0 leur imagination et fa\xE7onner des \u0153uvres d'art uniques.
+  <br /><br />
+  Th\xE9o, en qu\xEAte d'une nouvelle forme d'expression, avait toujours \xE9t\xE9 fascin\xE9 par la texture et la versatilit\xE9 de l'argile.
+  Pour lui, chaque morceau de terre glaise repr\xE9sentait une possibilit\xE9 infinie. De son c\xF4t\xE9, Romain trouvait dans la poterie une \xE9chappatoire \xE0 la rigueur de son m\xE9tier.
+  L'argile, douce et r\xE9confortante, lui permettait de se reconnecter avec ses \xE9motions et de red\xE9couvrir la simplicit\xE9 du toucher.
+  <br /><br />
+  Lors de leur premi\xE8re rencontre, leurs regards se crois\xE8rent au-dessus de leurs tours de potier respectifs.
+  Les mains de Th\xE9o, habiles et d\xE9licates, donnaient vie \xE0 une \xE9l\xE9gante sculpture, tandis que celles de Romain modelaient un vase aux courbes harmonieuses.
+  Le silence de l'atelier \xE9tait ponctu\xE9 par le doux murmure des tours en rotation et le chuchotement de l'eau qui glissait sur l'argile.
+  <br /><br />
+  Intrigu\xE9 par le travail de Romain, Th\xE9o s'approcha pour mieux observer.
+  Il fut imm\xE9diatement captiv\xE9 par la pr\xE9cision et la sensibilit\xE9 du geste. De son c\xF4t\xE9, Romain ressentit une connexion imm\xE9diate en voyant les \u0153uvres d\xE9licates de Th\xE9o, empreintes de passion et de po\xE9sie.
+  Leurs discussions commenc\xE8rent autour de l'argile, mais rapidement, elles s'\xE9tendirent \xE0 des sujets plus personnels. Ils partag\xE8rent leurs r\xEAves, leurs peurs, et les raisons profondes qui les avaient conduits jusqu'\xE0 cet atelier.
+  <br /><br />
+  Au fil des jours, la complicit\xE9 entre Th\xE9o et Romain se renfor\xE7a.
+  Ils se surprirent \xE0 attendre avec impatience chaque nouvelle s\xE9ance de poterie, non seulement pour cr\xE9er, mais aussi pour se retrouver.
+  Leurs mains, auparavant d\xE9di\xE9es \xE0 fa\xE7onner l'argile, se trouvaient maintenant \xE0 effleurer celles de l'autre, leur toucher devenant une forme de langage silencieux.
+  Les moments pass\xE9s ensemble \xE9taient empreints d'une douce tension, o\xF9 chaque geste, chaque regard portait en lui une promesse non formul\xE9e.
+  <br /><br />
+  Un soir, alors que le soleil se couchait et baignait l'atelier d'une lumi\xE8re dor\xE9e, Th\xE9o proposa \xE0 Romain de prolonger leur temps de travail pour finir une pi\xE8ce particuli\xE8rement complexe.
+  Seuls dans l'atelier, une nouvelle \xE9nergie les envahit. Les mots devinrent superflus, et ce fut naturellement que leurs mains se rejoignirent, cette fois non pas pour travailler l'argile, mais pour sceller leur passion naissante.
+  Les l\xE8vres de Th\xE9o trouv\xE8rent celles de Romain dans un baiser doux et fervent, exprimant tout ce que les mots ne pouvaient pas dire.
+  <br /><br />
+  \xC0 partir de ce moment, leur relation prit une nouvelle dimension.
+  La passion pour l'argile devint le socle de leur amour, chaque cr\xE9ation marquant une \xE9tape de leur histoire.
+  Leurs \u0153uvres, autrefois individuelles, devinrent des collaborations intimes, chaque pi\xE8ce portant en elle la trace de leurs deux \xE2mes m\xEAl\xE9es.
+  Th\xE9o et Romain d\xE9couvrirent que, tout comme l'argile, leur amour pouvait \xEAtre fa\xE7onn\xE9, enrichi et sublim\xE9 par la patience, la passion et le d\xE9sir.
+  <br /><br />
+  Ensemble, ils continu\xE8rent \xE0 explorer les infinies possibilit\xE9s de l'argile, transformant non seulement la mati\xE8re, mais aussi leurs vies.
+  L'atelier devint leur sanctuaire, un lieu o\xF9 l'art et l'amour se confondaient, o\xF9 chaque jour apportait son lot de d\xE9couvertes et de tendres complicit\xE9s.
+  Ainsi, dans la douceur et la chaleur de Marcq-en Baroeul, Th\xE9o et Romain trouv\xE8rent non seulement la passion de l'argile, mais surtout celle de leurs c\u0153urs entrelac\xE9s.
+</p>
+`,r=`
+<p>
+Nara \xE9tait une femme \xE0 la fois myst\xE9rieuse et chaleureuse, connue dans son village pour son amour incommensurable pour les chats. Depuis son enfance, elle avait toujours eu une affinit\xE9 particuli\xE8re avec ces cr\xE9atures \xE9l\xE9gantes et ind\xE9pendantes. Sa petite maison, perch\xE9e au sommet d'une colline, \xE9tait un v\xE9ritable refuge pour les f\xE9lins abandonn\xE9s et errants.
+<br />
+Les habitants de son village avaient pris l'habitude de voir Nara se promener, suivie d'une ribambelle de chats de toutes tailles et couleurs. Pour certains, c'\xE9tait un spectacle \xE9trange, mais pour ceux qui connaissaient Nara, c'\xE9tait une manifestation de sa compassion infinie et de son c\u0153ur d'or. Elle avait ce don singulier de comprendre et de communiquer avec les chats, les apaisant par sa simple pr\xE9sence.
+<br />
+Un jour, alors qu'elle se rendait au march\xE9 pour acheter de la nourriture pour ses prot\xE9g\xE9s, les gens du village remarqu\xE8rent une nouvelle lumi\xE8re dans ses yeux. Il semblait que Nara \xE9tait en paix avec elle-m\xEAme, trouvant son bonheur dans le quotidien simple et riche en \xE9motions qu'elle partageait avec ses compagnons f\xE9lins. La maison de Nara, avec son jardin luxuriant et ses coins tranquilles, \xE9tait devenue un sanctuaire non seulement pour les chats, mais aussi pour toute \xE2me en qu\xEAte de r\xE9confort.
+<br />
+Les ann\xE9es pass\xE8rent, et la maison de Nara devint non seulement un refuge pour les chats, mais aussi un lieu de s\xE9r\xE9nit\xE9 et de r\xE9confort pour les gens du village. Les enfants aimaient venir \xE9couter les histoires que Nara racontait sur chacun de ses chats, tandis que les adultes trouvaient en elle une confidente sage et compr\xE9hensive. La connexion unique entre Nara et ses compagnons f\xE9lins touchait tous ceux qui avaient la chance de la conna\xEEtre.
+<br />
+\xC0 mesure que Nara vieillissait, son amour pour les chats ne faisait que cro\xEEtre. Elle savait que sa vie \xE9tait pleinement accomplie, entour\xE9e de ces cr\xE9atures qu'elle aimait tant. Un soir, alors qu
+elle s'endormait dans son fauteuil pr\xE9f\xE9r\xE9, entour\xE9e de ses amis \xE0 quatre pattes, elle ressentit une paix profonde. Ses chats, sentant quelque chose de diff\xE9rent, se blottirent encore plus pr\xE8s d'elle, comme pour lui tenir compagnie dans ses derniers moments.
+<br />
+Nara s'\xE9teignit paisiblement, avec un sourire sur les l\xE8vres, consciente d'avoir v\xE9cu une vie riche et pleine de sens. Le lendemain, les villageois trouv\xE8rent la maison silencieuse mais impr\xE9gn\xE9e d'une douce qui\xE9tude. Les chats, en deuil de leur amie bien-aim\xE9e, rest\xE8rent pr\xE8s de son corps, veillant sur elle une derni\xE8re fois.
+<br />
+Profond\xE9ment touch\xE9s par la perte de Nara, les villageois d\xE9cid\xE8rent de continuer son \u0153uvre en prenant soin des chats et en maintenant le refuge ouvert. Ils firent de la maison de Nara un sanctuaire d\xE9di\xE9 \xE0 sa m\xE9moire, o\xF9 les chats et les amoureux des f\xE9lins pouvaient trouver refuge et consolation.
+<br />
+Ainsi, Nara, m\xEAme apr\xE8s sa mort, continua \xE0 inspirer et \xE0 apporter du bonheur, son esprit vivant \xE0 travers chaque chat qu'elle avait sauv\xE9 et chaque personne touch\xE9e par son histoire. Et dans ce coin tranquille du Nord, la l\xE9gende de Nara et de ses milliers de chats perdura, un t\xE9moignage \xE9ternel de l'amour et de la compassion.
+</p>
+`;export{e as a,a as b,r as c};
--- chunk-EEP7JBIE.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-BS5TZODD.js";import"./chunk-O5XJ52TU.js";import"./chunk-EE4OEHPH.js";export{a as LoginComponent};
